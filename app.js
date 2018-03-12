507b3996d462a1925a3fbd601b5d6cc8bb663031
veera83372 <veeravenky222@gmail.com>
2018-03-12 12:19:27 +0530

    removed all commit history

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.initilize=function(pg){
     app.pg = pg;
 }
 app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res,next)=>{
+  res.render('veera.pug',{
+    user:'veera',
+    tiles:['1A','1B'],
+    shares:{'Sackson':2,'Zeta':3}
+  });
+})
 app.use(logger);
 app.post('/connect',connectDb);
 app.use(express.static('public'));
